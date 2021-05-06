2777e0a8bbae6c9bc963eaa85758db3e5f867cc9
sturko1 <sturko1@etf.unsa.ba>
2021-05-06 14:08:57 +0200

    Dodan prazan VS projekat sa ispravnom autentifikacijom

--- Kompas/Kompas/Controllers/HomeController.cs
+++ Kompas/Kompas/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using Kompas.Models;
+﻿using KomPas.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -7,7 +7,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Kompas.Controllers
+namespace KomPas.Controllers
 {
     public class HomeController : Controller
     {
--- Kompas/Kompas/Models/ErrorViewModel.cs
+++ Kompas/Kompas/Models/ErrorViewModel.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace Kompas.Models
+namespace KomPas.Models
 {
     public class ErrorViewModel
     {
